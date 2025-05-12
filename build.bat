a873ae053109feb1c88405a842e07cab1bb20ed0
RejzaCode <filipjirku1@email.cz>
2025-05-12 15:58:47 +0200

    Add build.bat 1747058325892

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
 @echo off
 echo Installing dependencies...
-call npm install
+call npm install --legacy-peer-deps
+
+echo Installing Next.js globally...
+call npm install -g next
 
 echo Building the application...
-call npm run electron-build
+call npm run electron-build --legacy-peer-deps
 
 echo.
 echo Build complete! You can find the executable in the dist folder.
